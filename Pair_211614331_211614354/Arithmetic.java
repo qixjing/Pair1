8defab47dab8bffecb18dc35ca6d245210482b70
HYOUKACC <HYOUKACC@users.noreply.github.com>
2018-09-16 01:01:48 +0800

    功能整合：实现带括号的四则混合运算
    整合 找括号类 以及 四则运算类；
    可以计算带括号的四则运算题目；
    尚未加入年级限制；
    尚未考虑除法数据类型；
    规范代码格式；
    完善注释；

--- Pair_211614331_211614354/Arithmetic.java
+++ Pair_211614331_211614354/Arithmetic.java
@@ -2,85 +2,91 @@ import java.util.LinkedList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Arithmetic {        //四则混合运算
+public class Arithmetic extends Operation{                                           // 四则混合运算
+	
 
 	
-	public static void test() {   
-		//转换字符串，把多个字符变成有意义的数字
-		String str="1*9-9+695/865";
-		//56/8-6*8+6
-		//35-69+10*5/8
-		//8+9*8*8/6
-		//1-2+3+4
-		Matcher m=Pattern.compile("(\\d+)").matcher(str); //找数字
-		Matcher m2=Pattern.compile("[\\+\\-\\*\\/]").matcher(str); // 找符号
-		
-		int firstNumber=0;
-		int secondNumber=0;
-		int answerNumber=0;
-		
-		LinkedList<String> li=new LinkedList<String>(); // 存数字
-		LinkedList<String> li2=new LinkedList<String>();//存符号
-
-
-		int s;
-		
-		while(m.find()) {			
-			li.add(m.group());  //存数字或者存符号在集合里
-		}
-		System.out.println(li);
-	
-		while(m2.find()) {    //存符号
-			li2.add(m2.group());				
-		}
-		System.out.println(li2);
-		
-		for(int i=0;i<li2.size();i++) {
-			
-			if(li2.get(i).equals("*") || li2.get(i).equals("/")) {
-				
-				firstNumber=Integer.valueOf(li.get(i));               //找数字集合的数
-				secondNumber=Integer.valueOf(li.get(i+1)); 
-				
-				if(li2.get(i).equals("*")) {           //算乘法
-					answerNumber=firstNumber*secondNumber;
-					
-				}
-				else if(li2.get(i).equals("/")) {      //算除法
-					answerNumber=firstNumber/secondNumber;
-					
-				}
-				li.set(i, String.valueOf(answerNumber));
-				li.remove(i+1);//删掉后一个数
-				li2.remove(i);
-				i--;
-			}
-		}
-		
-		for(int i=0;i<li2.size();i++) {
-			
-			if(li2.get(i).equals("+") || li2.get(i).equals("-")) {
-				
-				firstNumber=Integer.valueOf(li.get(i));               //找数字集合的数
-				secondNumber=Integer.valueOf(li.get(i+1)); 
-				
-				if(li2.get(i).equals("+")) {           //算加法
-					answerNumber=firstNumber+secondNumber;
-					
-				}
-				else if(li2.get(i).equals("-")) {      //算减法
-					answerNumber=firstNumber-secondNumber;
-					
-				}
-				li.set(i, String.valueOf(answerNumber));
-				li.remove(i+1);//删掉后一个数
-				li2.remove(i);
-				i--;
-			}
-		}
-		System.out.println(li);
-		System.out.println(li2);
-		
-		
+	public Arithmetic(String sPart) {
+		setAnswerNumber(Integer.valueOf(arithmeticOne(sPart)));
 	}
+		
+	public static  String arithmeticOne(String sPart) {                                     // 混合运算一个题目   
+		
+		// (本方法的目标)转换字符串，把多个字符变成有意义的数字 
+		// 并返回当前题目的答案给找 括号类(FindBracket)
+				
+		final Matcher findNumber = Pattern.compile("(\\d+)").matcher(sPart);               // 正则查找字符串中的数字
+		final Matcher findSymbol = Pattern.compile("[\\+\\-\\*\\/]").matcher(sPart);     //  正则查找字符串中的符号
+			
+		LinkedList<String> numberSet = new LinkedList<String>();                   // 存数字的集合
+		LinkedList<String> symbolSet = new LinkedList<String>();					 // 存符号的集合
+	
+		while(findNumber.find()) {													// 开始找字符串中的数字
+			numberSet.add(findNumber.group());  									// 把找到的数字存在数字集合里
+		}
+																							
+		while(findSymbol.find()) {    													// 开始找字符串中的符号
+			symbolSet.add(findSymbol.group());										// 把找到的符号存在符号集合里
+		}
+																								
+		multiplicationAndDivision(numberSet,symbolSet);							// 先乘除后加减！！！
+		addAndSubtract(numberSet,symbolSet);									// 调用加减运算 
+				
+		 System.out.println(" 运算：" + answerNumber);
+		return numberSet.get(0);                									// 得到了当前题目的最终答案
+				
+	}
+	
+	public static void multiplicationAndDivision(LinkedList<String> numberSet, LinkedList<String> symbolSet) {
+		
+		for (int i = 0 ; i < symbolSet.size(); i++) {
+		//(目的) 遍历符号集合（本次只遍历*或/符号，因为先乘除后加减）
+		// 匹配完一个符号，算完结果，把结果加入数字集合，然后从符号集合中删除此符号
+
+			if (symbolSet.get(i).equals("*") || symbolSet.get(i).equals("/")) { 				// 当遍历出*或/时
+
+				firstNumber = Integer.valueOf(numberSet.get(i)); 						// 找数字集合中当前位置的数
+				secondNumber = Integer.valueOf(numberSet.get(i + 1)); 					// 再找到第二个数
+
+				if (symbolSet.get(i).equals("*")) { 											// 判断当前符号集合的符号，算乘法
+					answerNumber = firstNumber * secondNumber; 						// 算结果，保存于answerNumber中
+																																						
+				} else if (symbolSet.get(i).equals("/")) { 									// 算除法
+					answerNumber = firstNumber / secondNumber;
+					
+				}
+				
+				numberSet.set(i, String.valueOf(answerNumber)); 							// 向数字集合的第i个位置，添加答案，会覆盖掉firstNumber
+				numberSet.remove(i + 1); 													// 删掉数字集合中的secondNUmber
+				symbolSet.remove(i); 														// 当前符号已经运算完成，从符号集合中删去此符号，减少后期遍历集合次数
+				i--; 																			// 因为当前位置的符号已经删除了，集合会自动向前补齐，所以要找下一个符号，就需要把位置-1
+			}
+		}
+	}
+
+	public static void addAndSubtract(LinkedList<String> numberSet, LinkedList<String> symbolSet) {
+		
+		for (int i = 0; i < symbolSet.size(); i++) { 										// 运算加减，同理
+
+			if (symbolSet.get(i).equals("+") || symbolSet.get(i).equals("-")) {
+				
+				firstNumber = Integer.valueOf(numberSet.get(i)); 					// 找数字集合的数
+				secondNumber = Integer.valueOf(numberSet.get(i + 1));
+
+				if (symbolSet.get(i).equals("+")) { 										// 算加法
+					answerNumber = firstNumber + secondNumber;
+																																	
+				} else if (symbolSet.get(i).equals("-")) { 								// 算减法
+					answerNumber = firstNumber - secondNumber;
+																																											
+				}
+				
+				numberSet.set(i, String.valueOf(answerNumber));
+				numberSet.remove(i + 1); 												// 删掉后一个数
+				symbolSet.remove(i);
+				i--;
+			}
+		}
+	}
+	
 }
--- Pair_211614331_211614354/FindBracket.java
+++ Pair_211614331_211614354/FindBracket.java
@@ -1,81 +1,71 @@
-import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.LinkedList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-public class FindBracket {
+public class FindBracket extends Operation{                            // 找呀找呀找括号！！！
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ts();
-	}
+	static int sRight = 0; 											// 记录右括号的位置
+	static int sLeft = 0; 											// 记录左括号的位置
+	static String sPart = ""; 										// 一部分题目，即左右括号之间的题目
+	static LinkedList<String> sInitialProblemSet;   					// 初始题目集合（用来运算括号，以后方便删除内容）
+	static String sProblem; 												// 用来保存最初的题目的字符串
 
-	public static void ts() {
-		//
-		String s1="(A+B)-(C+D)*(B+A)";  
-		// ((A+B)/(C+D-B))*A
-		//((A+B)*(C+D)-A)/B
-		//(A+B)-(C+D)*(B+A)
-		
-		ArrayList<String> a1=new ArrayList<>();// 初始题目（用来运算括号以后方便删除内容）
-		
-		LinkedList<String> l1=new LinkedList<>();
-		for(int i=0;i<s1.length();i++) {
-			l1.add(String.valueOf(s1.charAt(i)));
+	
+
+	public  void findOne(String sProblem) {                              // 找到一个小括号！！！
+			
+		LinkedList<String> sInitialProblemSet = new LinkedList<String>(); 		// 初始题目（用来运算括号以后方便删除内容）
+				
+		for(int i = 0; i < sProblem.length(); i++) {                    				// 把接受到的题目转存到初始题目的集合中
+			sInitialProblemSet.add(String.valueOf(sProblem.charAt(i)));
 		}
 		
-		/*
-		 LinkedList<Integer> left=new LinkedList<>();
-		ArrayDeque<Integer> right=new ArrayDeque<>();
-		 */
-		
-		
-		Matcher m=Pattern.compile("\\(").matcher(s1);
-		Matcher m2=Pattern.compile("\\)").matcher(s1);
-	
-		int right=0; //右括号的位置
-		int left=0; //左括号的位置
-		
-		for(int i=0;i<l1.size();i++) {
-			if( l1.get(i).equals(")")) {
-				right=i;
-				for(int j=right-1;j>=0;j--) {
-					if(l1.get(j).equals("(")) {
-						l1.set(j, "!");
-						left=j;
+		for(int i = 0; i < sInitialProblemSet.size(); i++) { 						// 遍历集合，找到一个右括号
+			
+			
+			if( sInitialProblemSet.get(i).equals(")")) {    					// 当前位置是右括号时
+					sRight = i;   												// 记录右括号的位置
+				
+				for(int j = sRight-1; j >= 0; j--) {         						// 从当前右括号往前找第一个左括号
+					
+					if(sInitialProblemSet.get(j).equals("(")) {
+						sInitialProblemSet.set(j, "!");						// 改变标志左括号
+						sLeft = j;             									// 记录左括号位置
 						break;
 					}
 				}
 				
-				for(int z=left;z<=right;z++) {
-					System.out.print(l1.get(z));
-					//l1.remove(z);						
-				}
-				System.out.println();
+				for( int z = sLeft + 1; z < sRight; z++)							// 循环遍历，找出左右括号之间的数
+				{   						
+					sPart = sPart + sInitialProblemSet.get(z);					// 把找出来的数拼接起来，作为括号内题目的一部分
 					
-			}
-		}
-		
-		
-		
-		/*
-		while(m2.find()) {     //先找右括号，定位后，从当前位置往左扫描，找到第一个左括号
-			
-			                                                   //right.addLast(m2.start());
-			right=m2.start();          //括号的位置
-			for(int i=right-1;i>=0;i--) {
-				if( (String.valueOf(s1.charAt(i))).equals("(") ) {
-					left=i;      // 最后一个小于右括号位置的 左括号的位置
-					break;
 				}
-			}
-		
-				System.out.println(s1.substring(left, right+1));
+				
+				@SuppressWarnings("unused")
+				Arithmetic ar = new Arithmetic(sPart);						// 把sPart作为参数，传入四则运算类
+																			
+				sInitialProblemSet.set( sLeft, String.valueOf(getAnswerNumber()) );
+																				
+				for(int m = 1; m <= sRight-sLeft ; m++){
+																
+					sInitialProblemSet.remove(sLeft + 1);
+					
+				}
+																				
+				i = i - ( sRight - sLeft ); 								// 删除了sRight-sLeft个元素，所以i的位置也有重新移动
 						
-		}                                  //右括号
-		*/
-		
-		
-	}
+										// 删除后修正i的位置
+				
+			}//当前位置是右括号时					
+			
+			sPart="";															// 因为是拼接字符串，所以每找到一个括号算完后，就有要把字符串给清空
+		}// 找到右括号
+			
+		for(int i = 0; i < sInitialProblemSet.size(); i++) {			// 把最后无括号的式子传入四则运算符号
+			sPart = sPart + sInitialProblemSet.get(i);
+		}
+		@SuppressWarnings("unused")
+		Arithmetic arS = new Arithmetic(sPart);
+																	
+	}//findOne
+	
+	
 }
