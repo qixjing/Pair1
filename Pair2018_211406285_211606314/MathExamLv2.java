de6a2a7932a11c25e5f9302282c950fe0ccbd21e
a139520017 <toddle520@vip.qq.com>
2018-09-16 14:22:39 +0800

    初步完成代码，四则运算中的错误排查还没有进行

--- Pair2018_211406285_211606314/MathExamLv2.java
+++ Pair2018_211406285_211606314/MathExamLv2.java
@@ -10,45 +10,134 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.Random;
 
 public class MathExamLv2 {
-	private Object[] stack;
-	private int size;// 元素个数;
+	public static String answer="";
+    //用于记录操作符
+    private static LinkedList<String> operators=new LinkedList<>();
+    //用于记录输出
+    private static LinkedList<String> output=new LinkedList<>();
+    //用于展示后缀表达式
+    private static StringBuilder sb=new StringBuilder();
+    //中缀表达式转为后缀表达式
+    private static void transferToPostfix(LinkedList<String> list){
+        Iterator<String> it=list.iterator();
+        while (it.hasNext()) {
+            String s = it.next();
+            if (isOperator(s)) {
+                if (operators.isEmpty()) {
+                    operators.push(s);
+                }
+                else {
+                    //如果读入的操作符为非")"且优先级比栈顶元素的优先级高或一样，则将操作符压入栈
+                    if (priority(operators.peek())<=priority(s)&&!s.equals(")")) {
+                        operators.push(s);
+                    }
+                    else if(!s.equals(")")&&priority(operators.peek())>priority(s)){
+                        while (operators.size()!=0&&priority(operators.peek())>=priority(s)
+                                &&!operators.peek().equals("(")) {
+                            if (!operators.peek().equals("(")) {
+                                String operator=operators.pop();
+                                sb.append(operator).append(" ");
+                                output.push(operator);
+                            }
+                        }
+                        operators.push(s);
+                    }
+                    //如果读入的操作符是")"，则弹出从栈顶开始第一个"("及其之前的所有操作符
+                    else if (s.equals(")")) {
+                        while (!operators.peek().equals("(")) {
+                            String operator=operators.pop();
+                            sb.append(operator).append(" ");
+                            output.push(operator);
+                        }
+                        //弹出"("
+                        operators.pop();
+                    }
+                }
+            }
+            //读入的为非操作符
+            else {
+                sb.append(s).append(" ");
+                output.push(s);
+            }
+        }
+        if (!operators.isEmpty()) {
+            Iterator<String> iterator=operators.iterator();
+            while (iterator.hasNext()) {
+                String operator=iterator.next();
+                sb.append(operator).append(" ");
+                output.push(operator);
+                iterator.remove();
+            }
+        }
+        
+        System.out.println("后缀： "+sb);
+        calculate();
+        //Collections.reverse(output);
+    }
 
-	public MathExamLv2() {// 默认长度为10;
-		this(10);
-	}
+    //根据后缀表达式计算结果
+    private static void calculate(){
+        LinkedList<String> mList=new LinkedList<>();
+        String[] postStr=sb.toString().split(" ");
+        for (String s:postStr) {
+            if (isOperator(s)){
+                if (!mList.isEmpty()){
+                    int num1=Integer.valueOf(mList.pop());
+                    int num2=Integer.valueOf(mList.pop());
+                   /* if (s.equals("/")&&num1==0){
+                        System.out.println("除数不能为0");
+                        return;
+                    }*/
+                    int newNum=cal(num2,num1,s);
+                    mList.push(String.valueOf(newNum));
+                }
+            }
+            else {
+                //数字则压入栈中
+                mList.push(s);
+            }
+        }
+        if (!mList.isEmpty()){
+            answer=mList.pop();
+        }
+        sb.setLength(0);
+    }
 
-	public MathExamLv2(int len) {// 也可以自己设置长度,即容量;
-		stack = new Object[len];
-	}
+    //判断是否操作符
+    private static boolean isOperator(String oper){
+        if (oper.equals("+")||oper.equals("-")||oper.equals("/")||oper.equals("*")
+                ||oper.equals("(")||oper.equals(")")) {
+            return true;
+        }
+        return false;
+    }
+    //计算操作符的优先级
+    private static int priority(String s){
+        switch (s) {
+            case "+":return 1;
+            case "-":return 1;
+            case "*":return 2;
+            case "/":return 2;
+            case "(":return 3;
+            case ")":return 3;
+            default :return 0;
+        }
+    }
 
-	public int size() {// 返回元素个数;
-		return size;
-	}
-
-	public int capacity() {// 返回数组长度,即容量;
-		return stack.length;
-	}
-
-	public void push(Object o) {// 入栈;
-		size++;
-		stack[size - 1] = o;
-	}
-
-	public boolean isEmpty() {// 判空;
-		return size == 0;
-	}
-
-	public Object pop() {// 出栈;
-		if (isEmpty()) {// 首先要判空;
-			throw new ArrayIndexOutOfBoundsException("不能为空");
-		}
-		Object o = stack[--size];
-		stack[size] = null;
-		return o;
-	}
+    private static int cal(int num1,int num2,String operator){
+        switch (operator){
+            case "+":return num1+num2;
+            case "-":return num1-num2;
+            case "*":return num1*num2;
+            case "/":return num1/num2;
+            default :return 0;
+        }
+    }
 
 	private static void gradeOne(int n) throws IOException, FileNotFoundException {
 		int[] sum = new int[n + 1];// 创建sum数组用来保存答案
@@ -79,7 +168,7 @@ public class MathExamLv2 {
 			str = str + " " + a + " " + sign + " " + b + " " + "=";
 			out(str);
 			if (i == n) {
-				out("----------------标准答案----------------");
+				out("\r\n");
 			}
 		} // 这个for循环是用来确立要写入到out.txt中的str的值，也就是要出的题目
 
@@ -129,7 +218,7 @@ public class MathExamLv2 {
 			str = str + " " + a + " " + sign + " " + b + " " + "=";
 			out(str);
 			if (i == n) {
-				out("----------------标准答案----------------");
+				out("\r\n");
 			}
 		} // 这个for循环是用来确立要写入到out.txt中的str的值，也就是要出的题目
 
@@ -146,47 +235,63 @@ public class MathExamLv2 {
 	}
 
 	private static void gradeThree(int n) throws IOException, FileNotFoundException {
-		String[] sum = new String[n + 1];// 创建sum数组用来保存答案
-		char[] signSet = { '*', '/' };// 创建sianSet数组用来保存字符组
+		String[] sum = new String[n];
 		Random random = new Random();
-
-		for (int i = 1; i <= n; i++) {
-			String str = "(" + i + ")";
-			int a = random.nextInt(100) + 1;
-			int b = random.nextInt(100) + 1;
-			int t = random.nextInt(signSet.length);
-			char sign = signSet[t];
-
-			switch (sign) {
-			case '*':
-				sum[i] = String.valueOf(a * b);
-				sign = '×';
-				break;
-			case '/':
-				if (b == 0 && a > b) {
-					i = i - 1;
-					continue;
-				}
-				sum[i] = String.valueOf(a / b);
-				if (a % b != 0) {
-					sum[i] = sum[i] + "..." + String.valueOf(a % b);
-				}
-				sign = '÷';
-				break;
-			default:
-				;
+		MathExamLv2 stack = new MathExamLv2();
+		char[] signSet = { '*', '/' ,'+','-'};
+		for(int i=0;i<n;i++) {
+			int sumOfSign=random.nextInt(3)+1;
+			int flag=0;
+			if(sumOfSign>1) {
+				flag=random.nextInt(2);//令括号在其他运算符大于一个的时候有二分之一的概率出现
 			}
-			str = str + " " + a + " " + sign + " " + b + " " + "=";
+			int number[] = new int[4];
+			for(int j=0;j<4;j++) {
+				number[j] = random.nextInt(100) + 1;
+			}
+			String str= "(" + i + ")" + " " + number[0];
+			for(int k=0;k<sumOfSign;k++) {
+				str=str + " " + signSet[random.nextInt(4)] + " " + number[k+1];
+				if(k==sumOfSign-1) {
+					str=str+" ";
+				}
+			}
+			if((str.contains("-") || str.contains("+")) &&(str.contains("*") || str.contains("/"))) {
+				if(flag==1) {
+					int t=str.indexOf("-");
+					if(t==-1) {
+						t=str.indexOf("+");
+					}
+					int right=str.indexOf(" ", t+2);
+					int left=str.indexOf(" ",t-4);
+					StringBuffer sb = new StringBuffer(str);
+					sb.insert(right, " )");
+					sb.insert(left+1, "( ");
+					str=sb.toString();
+				}
+			}
+			LinkedList<String> list=new LinkedList<>();
+			String[] arr = str.split(" ");
+	        for(int q=1;q<arr.length;q++) {
+	        	if(q==1) {
+	        		list.add(arr[q]);
+	        		continue;
+	        	}
+	        	list.add(arr[q]);
+	        }
+			transferToPostfix(list);
+			sum[i]=answer;
+			list.clear();
+			arr=null;
+			str=str.replace('*', '×');
+			str=str.replace('/', '÷');
 			out(str);
-			if (i == n) {
-				out("----------------标准答案----------------");
-			}
-		} // 这个for循环是用来确立要写入到out.txt中的str的值，也就是要出的题目
-
+		}
+		out("\r\n");
 		BufferedReader br = in();
 
-		for (int i = 1; i <= n; i++) {
-			String str = br.readLine() + " " + sum[i];
+		for (int i = 0; i < n; i++) {
+			String str = br.readLine() + "=" + " " + sum[i];
 			out(str);
 			if (i == n) {
 				SimpleDateFormat df = new SimpleDateFormat("yyyy年-MM月-dd日 HH:mm");// 设置日期格式
