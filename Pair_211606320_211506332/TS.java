b55a260bff1bd8f3e4de9924ca36b9fff19737da
228353317 <42884536+228353317@users.noreply.github.com>
2018-09-18 23:03:15 +0800

    小学生出题器（改）
    适用三年级以内

---
+++ Pair_211606320_211506332/TS.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TS {
+	public static void main(String[] args) {
+		String[] str= {"-n","10","-grade","3"};
+		new MathExam().main(str);
+	}
+}
